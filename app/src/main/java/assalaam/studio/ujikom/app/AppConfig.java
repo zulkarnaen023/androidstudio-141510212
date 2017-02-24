134ff540d5d61565d9101a4e327d7a931fe5bfa5
sanditiasFikriyana <141510243@smkassalaambandung.sch.id>
2017-02-25 03:48:09 +0700

    android studio

---
+++ app/src/main/java/assalaam/studio/ujikom/app/AppConfig.java
@@ -0,0 +1,9 @@
+package assalaam.studio.ujikom.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://192.168.51.57/ujikom-141510212/public/api/login";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://ujikom-2017.smkassalaambandung.sch.id/api/register";
+}
